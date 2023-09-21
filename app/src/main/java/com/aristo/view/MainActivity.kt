e334fa23f83c53ca2c8520d88cd68265f9a3f682
SuMyat Thinzar <smtz1711@gmail.com>
2023-09-22 02:09:38 +0630

    Navigation Component with bottom navigation

--- app/src/main/java/com/aristo/view/MainActivity.kt
+++ app/src/main/java/com/aristo/view/MainActivity.kt
@@ -1,8 +1,27 @@
 package com.aristo.view
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.aristo.R
+import com.aristo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    // Create yon branch
+    private lateinit var binding : ActivityMainBinding
+    private lateinit var navController : NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+
+        NavigationUI.setupWithNavController(binding.bottomNav, navController)
+    }
 }
