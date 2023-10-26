8c5c35554994dcc5783df4ce6ec04c3cd98d05ad
YonThiri <yonthiriaung041998@gmail.com>
2023-10-26 22:41:13 +0630

    Add image to notification

--- app/src/main/java/com/aristo/Notification/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/aristo/Notification/MyFirebaseMessagingService.kt
@@ -1,21 +1,29 @@
 package com.aristo.Notification
 
+import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.NotificationManager.IMPORTANCE_HIGH
 import android.app.PendingIntent
 import android.app.PendingIntent.FLAG_ONE_SHOT
+import android.content.ContentResolver
 import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.graphics.Color
+import android.media.AudioAttributes
+import android.net.Uri
 import android.os.Build
+import android.util.Log
 import androidx.annotation.RequiresApi
 import androidx.core.app.NotificationCompat
 import com.aristo.R
 import com.aristo.view.MainActivity
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
+import java.net.URL
 import kotlin.random.Random
 
 private const val CHANNEL_ID = "my_channel"
@@ -51,40 +59,60 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
         }
 
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
-         val pendingIntent = PendingIntent.getActivity(this, 0, intent,
-            FLAG_ONE_SHOT or PendingIntent.FLAG_IMMUTABLE)
-        val notification = NotificationCompat.Builder(this, CHANNEL_ID)
-            .setContentTitle(message.data["title"])
-            .setContentText(message.data["message"])
+        val pendingIntent = PendingIntent.getActivity(this, 0, intent,
+            PendingIntent.FLAG_ONE_SHOT or PendingIntent.FLAG_IMMUTABLE)
+
+        val title = message.data["title"] ?: ""
+        val messageBody = message.data["message"] ?: ""
+        val imageUrl = message.data["image_url"] ?: ""
+
+        val notificationBuilder = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setContentTitle(title)
+            .setContentText(messageBody)
             .setSmallIcon(R.drawable.logo)
             .setAutoCancel(true)
             .setContentIntent(pendingIntent)
             .setPriority(NotificationCompat.PRIORITY_HIGH)
-            .build()
 
+        // Set big picture if imageUrl is provided
+        if (!imageUrl.isNullOrEmpty()) {
+            val bitmap = getBitmapFromUrl(imageUrl)
+
+            if (bitmap != null) {
+                notificationBuilder.setStyle(NotificationCompat.BigPictureStyle()
+                    .bigPicture(bitmap)
+                    .bigLargeIcon(null)) // Set null to remove large icon when big picture is displayed
+            }
+        }
+
+        val notification = notificationBuilder.build()
         notificationManager.notify(notificationID, notification)
     }
 
+    private fun getBitmapFromUrl(imageUrl: String): Bitmap? {
+        return try {
+            val url = URL(imageUrl)
+            BitmapFactory.decodeStream(url.openConnection().getInputStream())
+        } catch (e: Exception) {
+            e.printStackTrace()
+            null
+        }
+    }
+
+
     @RequiresApi(Build.VERSION_CODES.O)
     private fun createNotificationChannel(notificationManager: NotificationManager) {
+
         val channelName = "channelName"
-        val channel = NotificationChannel(CHANNEL_ID, channelName, IMPORTANCE_HIGH).apply {
+        val channel = NotificationChannel(CHANNEL_ID, channelName, NotificationManager.IMPORTANCE_HIGH).apply {
             description = "My channel description"
             enableLights(true)
+            setSound(android.net.Uri.parse("android.resource://"
+                    + applicationContext.packageName + "/" + R.raw.notification),
+                Notification.AUDIO_ATTRIBUTES_DEFAULT)
             lightColor = Color.GREEN
         }
         notificationManager.createNotificationChannel(channel)
     }
 
 }
-
-
-
-
-
-
-
-
-
-
-
